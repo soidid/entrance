05a8212defb2d392d750c8227764304515c3ec80
soidid <soidid@gmail.com>
2014-10-13 23:29:02 +0800

    add news page.

---
+++ static/routes/tumblr.js
@@ -0,0 +1,93 @@
+var http = require('http'),
+    cheerio = require('cheerio');
+
+
+exports.index = function(req, res){
+  
+  var options = {
+      host: 'news.watchout.tw',
+      path: '/tagged/市長給問嗎',
+      method: 'GET'
+  };
+  var request = http.get(options, function(response) {
+      //console.log('STATUS: ' + res.statusCode);
+      //console.log('HEADERS: ' + JSON.stringify(res.headers));
+      var bodyChunks = [];
+      response.on('data', function(chunk) {
+          bodyChunks.push(chunk);
+
+      }).on('end', function() {
+          var body = Buffer.concat(bodyChunks);
+          //console.log('BODY: ' + body);
+          $ = cheerio.load(body);
+          
+          //console.log($.html('#posts'));
+          $('#blog_main_title').text("市長，給問嗎？新聞頻道");
+          $('#musou_link').remove();
+          $('.nav-menu').remove();
+
+          $('.read_more').each(function (i, elem) {
+              // reset link
+              $(this).attr('href', '/news/'+$(this).attr('href').split('post/')[1]);
+          });
+          
+          var parsedDOM = $.html('');
+          //send processed DOM
+          return res.render('news', { data:parsedDOM });
+        
+      })
+      //process DOM
+      response.on('error', function(err) {
+          res.send('error: ' + err.message);
+      });
+
+      
+   
+  });
+
+  
+  
+};
+
+exports.post = function(req, res){
+
+  var name = req.params.news_id;
+  var options = {
+      host: 'news.watchout.tw',
+      path: '/post/'+name,
+      method: 'GET'
+  };
+  console.log(name);
+  var request = http.get(options, function(response) {
+      //console.log('STATUS: ' + res.statusCode);
+      //console.log('HEADERS: ' + JSON.stringify(res.headers));
+      var bodyChunks = [];
+      response.on('data', function(chunk) {
+          bodyChunks.push(chunk);
+
+      }).on('end', function() {
+          var body = Buffer.concat(bodyChunks);
+          //console.log('BODY: ' + body);
+          $ = cheerio.load(body);
+          
+          //console.log($.html('#posts'));
+          $('#blog_main_title').text("市長，給問嗎？新聞頻道");
+          $('.nav-menu').remove();
+          //$('.read_more')
+          
+          var parsedDOM = $.html('');
+          //send processed DOM
+          return res.render('news', { data:parsedDOM });
+        
+      })
+      //process DOM
+      response.on('error', function(err) {
+          res.send('error: ' + err.message);
+      });
+
+   
+  });
+
+  
+  
+};
