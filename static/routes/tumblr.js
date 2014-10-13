d34fc4550faa942843ae108c297175a55183fd31
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2014-10-14 01:09:12 +0800

    improve single post

--- static/routes/tumblr.js
+++ static/routes/tumblr.js
@@ -3,7 +3,7 @@ var http = require('http'),
 
 
 exports.index = function(req, res){
-  
+
   var options = {
       host: 'news.watchout.tw',
       path: '/tagged/市長給問嗎',
@@ -20,7 +20,7 @@ exports.index = function(req, res){
           var body = Buffer.concat(bodyChunks);
           //console.log('BODY: ' + body);
           $ = cheerio.load(body);
-          
+
           //console.log($.html('#posts'));
           $('#blog_main_title').text("市長，給問嗎？新聞頻道");
           $('#musou_link').remove();
@@ -30,32 +30,37 @@ exports.index = function(req, res){
               // reset link
               $(this).attr('href', '/news/'+$(this).attr('href').split('post/')[1]);
           });
-          
+
           var parsedDOM = $.html('');
           //send processed DOM
           return res.render('news', { data:parsedDOM });
-        
+
       })
       //process DOM
       response.on('error', function(err) {
           res.send('error: ' + err.message);
       });
 
-      
-   
+
+
   });
 
-  
-  
+
+
 };
 
 exports.post = function(req, res){
-
+  var path;
   var name = req.params.news_id;
   var sub_id = req.params.sub_id;
+  if(sub_id) {
+    path = '/post/'+name+'/'+sub_id;
+  } else {
+    path = '/post/'+name;
+  }
   var options = {
       host: 'news.watchout.tw',
-      path: '/post/'+name+'/'+sub_id,
+      path: path,
       method: 'GET'
   };
   console.log(name);
@@ -70,25 +75,25 @@ exports.post = function(req, res){
           var body = Buffer.concat(bodyChunks);
           //console.log('BODY: ' + body);
           $ = cheerio.load(body);
-          
+
           //console.log($.html('#posts'));
           $('#blog_main_title').text("市長，給問嗎？新聞頻道");
           $('.nav-menu').remove();
           //$('.read_more')
-          
+
           var parsedDOM = $.html('');
           //send processed DOM
           return res.render('news', { data:parsedDOM });
-        
+
       })
       //process DOM
       response.on('error', function(err) {
           res.send('error: ' + err.message);
       });
 
-   
+
   });
 
-  
-  
+
+
 };
